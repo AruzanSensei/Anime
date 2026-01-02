2310afea7d338207916016d269c5aa37cb97b5ff
AruzanSensei <alzanadytia.j@gmail.com>
2026-01-02 16:41:30 +0700

    update API_URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 // Configuration
 const CONFIG = {
     // Update this URL after deploying backend to Railway
-    API_URL: 'http://localhost:3000',
+    // API_URL: 'http://localhost:3000',
     // Or use your Railway URL:
-    // API_URL: 'https://your-app-name.up.railway.app'
+    API_URL: 'anime-production-ed9b.up.railway.app'
 };
